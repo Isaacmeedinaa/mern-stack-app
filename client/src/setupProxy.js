b4991a0cbb6d6c2a7382463fee5803c4abffc359
Isaac Medina <isaac.meedinaa@gmail.com>
2020-12-04 22:25:29 -0600

    changes target proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,11 +1,11 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+// const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = (app) => {
-  app.use(
-    "/api",
-    createProxyMiddleware({
-      target: "http://mern-stack-app147.herokuapp.com",
-      changeOrigin: true,
-    })
-  );
-};
+// module.exports = (app) => {
+//   app.use(
+//     "/api",
+//     createProxyMiddleware({
+//       target: "http://localhost:5000",
+//       changeOrigin: true,
+//     })
+//   );
+// };
